04740905dd6cbbc771fe5ed56a2a9e8f49de8308
iris qu <iris.xiaoyu.qu@gmail.com>
2016-03-01 18:04:28 -0500

    commit2

--- groupY/src/ofApp.cpp
+++ groupY/src/ofApp.cpp
@@ -1,18 +1,46 @@
 #include "ofApp.h"
 
+void ofApp::drawShape(float diam) {
+    
+    
+    ofSetColor(abs(sin(ofGetElapsedTimef()/3))*255, abs(cos(ofGetElapsedTimef()/2))*255, abs(cos(ofGetElapsedTimef()/4))*255, 255);
+    
+    ofNoFill();
+    
+    ofRect(0, 0, 50, diam);
+    
+    diam = diam*1.007;
+    
+    if(diam < 1200){
+        
+        ofPushMatrix();
+        ofRotate(2);
+        drawShape(diam);
+        ofPopMatrix();
+        
+    }
+}
+
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+    ofBackground(0);
+    ofSetBackgroundAuto(false);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    ofSetColor(0, 0, 0, 20);
+    ofFill();
+    ofRect(0, 0, ofGetWidth(), ofGetHeight());
+    
+    ofTranslate(ofGetWidth()/2, ofGetHeight()/2);
+   
+    drawShape(abs(cos(ofGetElapsedTimef()/7))*100);
 }
 
 //--------------------------------------------------------------
